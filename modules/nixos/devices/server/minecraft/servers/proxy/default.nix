52c0336a80634d81a4285981b6ed3e1612e68597
Josef Katič <josef@joka00.dev>
2024-08-30 00:39:09 +0200

    server/minecraft: Update packages

--- modules/nixos/devices/server/minecraft/servers/proxy/default.nix
+++ modules/nixos/devices/server/minecraft/servers/proxy/default.nix
@@ -25,7 +25,7 @@ in {
         echo 'velocity reload' > /run/minecraft-server/proxy.stdin
       '';
 
-      package = inputs.nix-minecraft.velocity-server; # Latest build
+      package = inputs.nix-minecraft.packages.${pkgs.system}.velocity-server; # Latest build
       jvmOpts = proxyFlags "1G";
 
       files = {
