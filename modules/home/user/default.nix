ae70990e654deba70e95a5d044bbdc29e17f1b6d
Josef Katič <josef@joka00.dev>
2024-08-29 14:33:25 +0200

    home/users: Remove duplicit modules

--- modules/home/user/default.nix
+++ modules/home/user/default.nix
@@ -17,7 +17,6 @@ inputs: {
     inputs.impermanence.nixosModules.home-manager.impermanence
     inputs.matugen.nixosModules.default
     inputs.nix-index-db.hmModules.nix-index
-    inputs.nix-index-db.hmModules.nix-index
     inputs.nur.nixosModules.nur
     inputs.sops-nix.homeManagerModules.sops
   ];
