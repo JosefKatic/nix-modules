52e1c62fa29fe51e4dceedfb4e0c2a21b41b75e1
Josef Katič <josef@joka00.dev>
2024-11-01 12:28:03 +0100

    home/polkit-agent: Replace with hyprpolkit

--- modules/home/user/desktop/services/polkit-agent/default.nix
+++ modules/home/user/desktop/services/polkit-agent/default.nix
@@ -11,8 +11,8 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    systemd.user.services.polkit-gnome-authentication-agent-1 = {
-      Unit.Description = "polkit-gnome-authentication-agent-1";
+    systemd.user.services.hyprpolkitagent-1 = {
+      Unit.Description = "hyprpolkitagent-1";
 
       Install = {
         WantedBy = ["graphical-session.target"];
@@ -22,7 +22,7 @@ in {
 
       Service = {
         Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        ExecStart = "${pkgs.hyprpolkitagent}/libexec/hyprpolkitagent";
         Restart = "on-failure";
         RestartSec = 1;
         TimeoutStopSec = 10;
