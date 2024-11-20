6b84c2cec9e6c326d87b9ea90c694b9b718815ba
Josef Katič <josef@joka00.dev>
2024-11-20 02:46:55 +0100

    Try to fix aarch64

--- modules/nixos/devices/core/kernel.nix
+++ modules/nixos/devices/core/kernel.nix
@@ -16,11 +16,11 @@ in {
   config = {
     boot = {
       kernelPackages = pkgs.linuxKernel.packages.${config.device.core.kernel};
-      initrd = {
-        availableKernelModules =
-          lib.optionals config.device.virtualized ["ata_piix" "uhci_hcd" "virtio_pci" "sr_mod" "virtio_blk"]
-          ++ lib.optionals (config.device.virtualized != false) ["nvme" "xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc"];
-      };
+      # initrd = {
+      #   availableKernelModules =
+      #     lib.optionals config.device.virtualized ["ata_piix" "uhci_hcd" "virtio_pci" "sr_mod" "virtio_blk"]
+      #     ++ lib.optionals (config.device.virtualized != false) ["nvme" "xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc"];
+      # };
       extraModprobeConfig = lib.mkIf config.device.virtualized "options kvm nested=1";
     };
     services.fwupd.enable = true;
