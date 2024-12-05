2d2a2cbfb4ca19c5be2b43d744861529a8fea05b
Josef Katič <josef@joka00.dev>
2024-12-05 01:42:34 +0100

    Add joka00.dev certificate

--- modules/nixos/devices/server/homelab/default.nix
+++ modules/nixos/devices/server/homelab/default.nix
@@ -23,7 +23,7 @@
     device.server.services.web.acme.enable = true;
     security.acme.certs."joka00.dev" = {
       domain = "joka00.dev";
-      extraDomainNames = ["*.joka00.dev" "*.remote.joka00.dev"];
+      extraDomainNames = ["*.joka00.dev"];
       dnsProvider = "godaddy";
       dnsPropagationCheck = true;
       credentialsFile = config.sops.secrets.acme-secrets.path;
--- modules/nixos/devices/server/services/web/acme/default.nix
+++ modules/nixos/devices/server/services/web/acme/default.nix
@@ -10,6 +10,13 @@
     security.acme = {
       defaults.email = "josef+acme@joka00.dev";
       acceptTerms = true;
+      certs."joka00.dev" = {
+        domain = "joka00.dev";
+        extraDomainNames = ["*.joka00.dev"];
+        dnsProvider = "godaddy";
+        dnsPropagationCheck = true;
+        credentialsFile = config.sops.secrets.acme-secrets.path;
+      };
     };
 
     environment.persistence = lib.mkIf config.device.core.storage.enablePersistence {
