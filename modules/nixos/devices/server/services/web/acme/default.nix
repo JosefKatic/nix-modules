cccbfe72e6107c76f2afd3e73858ea09df8444ea
Josef Katič <josef@joka00.dev>
2024-12-05 20:29:34 +0100

    Add extra domain

--- modules/nixos/devices/server/services/web/acme/default.nix
+++ modules/nixos/devices/server/services/web/acme/default.nix
@@ -16,7 +16,7 @@
       acceptTerms = true;
       certs."joka00.dev" = {
         domain = "joka00.dev";
-        extraDomainNames = ["*.joka00.dev"];
+        extraDomainNames = ["*.joka00.dev" "*.devices.joka00.dev"];
         dnsProvider = "godaddy";
         dnsPropagationCheck = true;
         webroot = null;
