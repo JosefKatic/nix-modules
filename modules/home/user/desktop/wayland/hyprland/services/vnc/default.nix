9574296a2fdc9dd898b8301df92ddd72dad5e764
Josef Katič <josef@joka00.dev>
2024-12-17 16:59:12 +0100

    Add missing packages

--- modules/home/user/desktop/wayland/hyprland/services/vnc/default.nix
+++ modules/home/user/desktop/wayland/hyprland/services/vnc/default.nix
@@ -9,17 +9,15 @@
   vncsh = pkgs.writeShellScriptBin "vnc.sh" ''
     ssh $1 bash <<'EOF'
         pgrep "wayvnc" && exit
-        export HYPRLAND_INSTANCE_SIGNATURE="$(ls "$XDG_RUNTIME_DIR/hypr/" -lt | head -2 | tail -1 | rev | cut -d ' ' -f1 | rev)"
+        export HYPRLAND_INSTANCE_SIGNATURE="$(${pkgs.coreutils}/bin/ls "$XDG_RUNTIME_DIR/hypr/" -lt | head -2 | tail -1 | rev | cut -d ' ' -f1 | rev)"
         export WAYLAND_DISPLAY="wayland-1"
         ip="$(ip addr show dev tailscale0 | grep 'inet ' | xargs | cut -d ' ' -f2 | cut -d '/' -f1)"
-        xpos="$(hyprctl monitors -j | jq -r 'sort_by(.x)[-1] | .x + .width')"
-
         ${lib.concatLines (
       lib.forEach enabledMonitors (m: ''
         hyprctl output create headless
-        monitor="$(hyprctl monitors -j | jq -r 'map(.name)[-1]')"
+        monitor="$(hyprctl monitors -j | ${pkgs.jq} -r 'map(.name)[-1]')"
         hyprctl keyword monitor "$monitor,${toString m.width}x${toString m.height}@${toString m.refreshRate},${toString m.position},1"
-        screen -d -m wayvnc -k br -S /tmp/vnc-${m.workspace} -f 60 -o "$monitor" "$ip" 590${m.workspace}
+        ${pkgs.screen} -d -m wayvnc -k br -S /tmp/vnc-${m.workspace} -f 60 -o "$monitor" "$ip" 590${m.workspace}
         sudo iptables -I INPUT -j ACCEPT -p tcp --dport 590${m.workspace}
       '')
     )}
