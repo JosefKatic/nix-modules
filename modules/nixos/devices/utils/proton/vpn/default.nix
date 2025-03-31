f71d36f3979758c8abd1e7546e425937f476e18a
Josef Katič <josef@joka00.dev>
2025-04-01 01:21:58 +0200

    utils/proton: Fix syntax

--- modules/nixos/devices/utils/proton/vpn/default.nix
+++ modules/nixos/devices/utils/proton/vpn/default.nix
@@ -1,11 +1,12 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
   cfg = config.device;
 in {
   options.device.utils.proton.vpn.enable = lib.mkEnableOption "Enable Proton VPN";
 
-  config = {environment.systemPackages = with pkgs; [protonvpn-cli];};
+  config = {environment.systemPackages = [pkgs.protonvpn-cli];};
 }
