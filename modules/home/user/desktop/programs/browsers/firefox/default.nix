3ffea5381f2954132c639f934a3a8698e6462ad6
Josef Katič <josef@joka00.dev>
2025-04-01 14:29:39 +0200

    Migrate home-manager options

--- modules/home/user/desktop/programs/browsers/firefox/default.nix
+++ modules/home/user/desktop/programs/browsers/firefox/default.nix
@@ -87,7 +87,7 @@ in {
                 template = "https://nixos.wiki/index.php?search={searchTerms}";
               }
             ];
-            iconUpdateURL = "https://nixos.wiki/favicon.png";
+            icon = "https://nixos.wiki/favicon.png";
             updateInterval = 24 * 60 * 60 * 1000;
             definedAliases = ["@nw"];
           };
