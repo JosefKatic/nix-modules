dc2753fb36917bd923da20dc5aa7d1f769ce9c68
Josef Katič <josef@joka00.dev>
2025-04-01 13:26:28 +0200

    Add Proton Mail

---
+++ modules/home/user/desktop/programs/productivity/proton/mail/default.nix
@@ -0,0 +1,21 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}: let
+  cfg = config.user.desktop.programs.productivity.zathura;
+in {
+  options.user.desktop.programs.productivity.proton.pass = {
+    enable = lib.mkEnableOption "Enable Proton Pass";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = [
+      [
+        pkgs.protonmail-desktop
+        pkgs.protonmail-bridge
+      ]
+    ];
+  };
+}
