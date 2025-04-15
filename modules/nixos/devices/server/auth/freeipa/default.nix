4194687c132091ca483c9f17f0ca76edcdefca26
Josef Katič <josef@joka00.dev>
2025-04-15 13:24:01 +0200

    Add headers

--- modules/nixos/devices/server/auth/freeipa/default.nix
+++ modules/nixos/devices/server/auth/freeipa/default.nix
@@ -104,9 +104,12 @@ in {
         forceSSL = true;
         enableACME = true;
         locations."/" = {
-          proxyPass = http://localhost:8000;
+          proxyPass = http://ipa.auth.joka00.dev:8080;
           extraConfig = ''
             proxy_set_header        Host $host;
+            proxy_http_version      1.1;
+            proxy_set_header        Upgrade $http_upgrade;
+            proxy_set_header        Connection $connection_upgrade;
             proxy_set_header        X-Real-IP $remote_addr;
             proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
             proxy_set_header        X-Forwarded-Proto $scheme;
