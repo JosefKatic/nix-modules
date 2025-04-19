3b63544d2de37085f0c70fd8ee057f99488f1ef7
Josef Katič <josef@joka00.dev>
2025-04-19 16:35:48 +0200

    Use IPA certificate for config.internal.joka00.dev

--- modules/nixos/devices/server/nix-configurator/client.nix
+++ modules/nixos/devices/server/nix-configurator/client.nix
@@ -16,11 +16,25 @@ in {
     };
   };
   config = lib.mkIf cfg.enable {
+    sops.secrets = {
+      config_ssl_fullchain = {
+        sopsFile = "${self}/secrets/services/config/secrets.yaml";
+        owner = "nginx";
+        group = "nginxs";
+      };
+      config_ssl_key = {
+        sopsFile = "${self}/secrets/services/config/secrets.yaml";
+        owner = "nginx";
+        group = "nginx";
+      };
+    };
+
     device.server.nix-configurator.api.enable = lib.mkDefault true;
     services.nginx = {
       virtualHosts."config.internal.joka00.dev" = {
         forceSSL = true;
-        useACMEHost = "internal.joka00.dev";
+        sslCertificate = config.sops.secrets.config_ssl_fullchain.path;
+        sslCertificateKey = config.sops.secrets.config_ssl_key.path;
         locations."/" = {
           root = "${inputs.nix-configurator-web.packages.${pkgs.system}.default}";
           index = "index.html";
