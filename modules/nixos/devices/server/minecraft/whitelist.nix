b5b8e6c95c66418a2c21ead0e3a14d59dcf72d86
Josef Katič <josef@joka00.dev>
2025-05-23 20:06:07 +0200

    Update whitelist

--- modules/nixos/devices/server/minecraft/whitelist.nix
+++ modules/nixos/devices/server/minecraft/whitelist.nix
@@ -1,6 +1,6 @@
 # Whitelist for the private SMP servers (e.g. modpack)
 {
-  Pepik_CZ = "ba12f363-170c-314f-a43a-953b2335e4d0";
-  bazi007 = "3987d985-c243-38cd-aa6f-a0881869bc05";
-  qwik_ = "57e3934d-2a2f-35cc-b0ed-61f4f9e6d37c";
+  Pepik_CZ = "7f42900f-ea07-4a6b-9ee1-6a224f1843f6";
+  bazi007 = "f6e1d74a-c5a7-408c-aff1-23817ebc3dfd";
+  qwik_ = "2db0a8d2-8acb-4464-9936-8e8f533dc2da";
 }
