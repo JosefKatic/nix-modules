9c6eca51c4242f2237a9f46acca19aeda8fa639d
Josef Katič <josef@joka00.dev>
2025-05-23 13:20:02 +0200

    Return back to fetchzip

--- modules/nixos/devices/server/minecraft/servers/modpack/default.nix
+++ modules/nixos/devices/server/minecraft/servers/modpack/default.nix
@@ -1,15 +1,12 @@
 inputs: {pkgs, ...}: let
   inherit (inputs.nix-minecraft.lib) collectFilesAt;
-  mrpackSource = pkgs.fetchurl {
-    url = "https://cdn.modrinth.com/data/TK1lQFH6/versions/PXU2pZT5/Create%20%26%20Explore%20-%20pre2.1.0.mrpack";
+  modpack = pkgs.fetchzip {
+    url = "https://www.dropbox.com/scl/fi/lj7j77yt851h5vkq9ezln/modpack.zip?rlkey=xxoqjorlxuv04an7mznqavv8i&st=iz256o9y&dl=1";
     name = "create-and-explore";
-    sha256 = "sha256-1XxZ15LWWILICGE+s9kDedkMijzilLo/LWtu3E+nAHo=";
+    extension = "zip";
+    hash = "sha256-9PBKETqcE3iMbZEPCgMS2ZFWusxrW3ppO5MH10Ld7Kk=";
   };
   fabricServer = inputs.nix-minecraft.legacyPackages.${pkgs.system}.fabricServers.fabric-1_20_1.override {loaderVersion = "0.15.6";};
-  modpack = pkgs.runCommand "install-modpack" {} ''
-    mkdir -p $out
-    ${pkgs.mrpack-install}/bin/mrpack-install ${mrpackSource} --server-dir "$out" --server-file ${fabricServer}
-  '';
 in {
   services.minecraft-servers.servers.modpack = {
     enable = true;
@@ -36,27 +33,27 @@ in {
       config = "${modpack}/config";
       defaultconfigs = "${modpack}/defaultconfigs";
     };
-    # symlinks =
-    #   collectFilesAt modpack "mods"
-    #   // {
-    #     "mods/towns-and-towers.jar" = pkgs.fetchurl {
-    #       url = "https://cdn.modrinth.com/data/DjLobEOy/versions/7ZwnSrVW/Towns-and-Towers-1.12-Fabric%2BForge.jar";
-    #       name = "towns-and-towers";
-    #       extension = "jar";
-    #       hash = "sha256-nIEVr3EJV52pkCSf3WezgyOkW+cPijqWK2HaaccCGYQ='";
-    #     };
-    #     "mods/tectonic.jar" = pkgs.fetchurl {
-    #       url = "https://cdn.modrinth.com/data/lWDHr9jE/versions/SWDOp7uu/tectonic-3.0.0%2Bbeta4.jar";
-    #       name = "tectonic-3.0.0.beta4";
-    #       extension = "jar";
-    #       hash = "sha256-4IOczEPzaviDGZTlnA29ohRDsNI3j/bDRnEIx5C3cG4=";
-    #     };
-    #     "mods/terralith.jar" = pkgs.fetchurl {
-    #       url = "https://cdn.modrinth.com/data/8oi3bsk5/versions/WeYhEb5d/Terralith_1.20.x_v2.5.4.jar";
-    #       name = "terralith_v2.5.4";
-    #       extension = "jar";
-    #       hash = "ha256-j2XzCdjycjdUv0tgx7V2PTq27QSwHBchCbplZOmBuV8=";
-    #     };
-    #   };
+    symlinks =
+      collectFilesAt modpack "mods"
+      // {
+        "mods/towns-and-towers.jar" = pkgs.fetchurl {
+          url = "https://cdn.modrinth.com/data/DjLobEOy/versions/7ZwnSrVW/Towns-and-Towers-1.12-Fabric%2BForge.jar";
+          name = "towns-and-towers";
+          extension = "jar";
+          hash = "sha256-nIEVr3EJV52pkCSf3WezgyOkW+cPijqWK2HaaccCGYQ='";
+        };
+        "mods/tectonic.jar" = pkgs.fetchurl {
+          url = "https://cdn.modrinth.com/data/lWDHr9jE/versions/SWDOp7uu/tectonic-3.0.0%2Bbeta4.jar";
+          name = "tectonic-3.0.0.beta4";
+          extension = "jar";
+          hash = "sha256-4IOczEPzaviDGZTlnA29ohRDsNI3j/bDRnEIx5C3cG4=";
+        };
+        "mods/terralith.jar" = pkgs.fetchurl {
+          url = "https://cdn.modrinth.com/data/8oi3bsk5/versions/WeYhEb5d/Terralith_1.20.x_v2.5.4.jar";
+          name = "terralith_v2.5.4";
+          extension = "jar";
+          hash = "ha256-j2XzCdjycjdUv0tgx7V2PTq27QSwHBchCbplZOmBuV8=";
+        };
+      };
   };
 }
