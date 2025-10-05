16562a150cfa2730c36ca01b94a147fd5afa6c75
Josef Katič <josef@joka00.dev>
2025-10-05 23:08:39 +0200

    home: Change plasmaPackages with kdePackages

--- modules/home/user/desktop/services/kdeconnect/default.nix
+++ modules/home/user/desktop/services/kdeconnect/default.nix
@@ -13,7 +13,7 @@
       Unit.Description = "KDEConnect Service";
       Service = {
         Type = "simple";
-        ExecStart = "${pkgs.plasma5Packages.kdeconnect-kde}/bin/kdeconnect-indicator";
+        ExecStart = "${pkgs.kdePackages.kdeconnect-kde}/bin/kdeconnect-indicator";
         TimeoutStopSec = 5;
       };
       Install.WantedBy = ["graphical-session.target"];
