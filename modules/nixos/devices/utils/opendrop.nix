269c99a4fc160b833d389023e2050c5b578b57c4
Josef Katič <josef@joka00.dev>
2025-10-13 16:28:49 +0200

    Add OpenDrop support

---
+++ modules/nixos/devices/utils/opendrop.nix
@@ -0,0 +1,15 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.device.utils;
+in {
+  options.device.utils.opendrop.enable = lib.mkEnableOption "Enable OpenDrop";
+
+  config = lib.mkIf cfg.opendrop.enable {
+    environment.systemPackages = [
+      pkgs.opendrop
+    ];
+  };
+}
