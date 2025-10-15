180437b4f4e585c4194947696d4375f9900f2aa6
Josef Katič <josef@joka00.dev>
2025-10-15 13:08:30 +0200

    Add deps to quickshell

--- modules/home/user/desktop/wayland/quickshell/default.nix
+++ modules/home/user/desktop/wayland/quickshell/default.nix
@@ -3,8 +3,13 @@ inputs: {
   lib,
   pkgs,
   ...
-}: {
-  home.packages = with inputs; [
-    quickshell.packages.${pkgs.system}.default
+}: let
+  quickshell = inputs.quickshell.packages.${pkgs.system}.default;
+in {
+  home.packages = [quickshell];
+  home.sessionVariables.QML2_IMPORT_PATH = lib.concatStringsSep ":" [
+    "${quickshell}/lib/qt-6/qml"
+    "${pkgs.kdePackages.qtdeclarative}/lib/qt-6/qml"
+    "${pkgs.kdePackages.kirigami.unwrapped}/lib/qt-6/qml"
   ];
 }
