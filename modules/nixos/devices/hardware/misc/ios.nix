a67fe8fe64e290eb35a7d7022bc194ec5a58d369
Josef Katič <josef@joka00.dev>
2025-10-20 00:46:01 +0200

    Replace usbmuxd with usbmuxd2

--- modules/nixos/devices/hardware/misc/ios.nix
+++ modules/nixos/devices/hardware/misc/ios.nix
@@ -9,7 +9,10 @@ in {
   options.device.hardware.misc.ios.enable = lib.mkEnableOption "Enable iOS device support";
 
   config = lib.mkIf cfg.ios.enable {
-    services.usbmuxd.enable = true;
+    services.usbmuxd = {
+      enable = true;
+      package = pkgs.usbmuxd2;
+    };
 
     environment.systemPackages = with pkgs; [
       libimobiledevice
