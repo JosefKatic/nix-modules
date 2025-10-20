d3fa4747989da5b760612a7fca8558d2bae07c21
Josef Katič <josef@joka00.dev>
2025-10-20 17:36:25 +0200

    Migrate homeassistant to plain container

--- modules/nixos/devices/server/homelab/homeassistant/default.nix
+++ modules/nixos/devices/server/homelab/homeassistant/default.nix
@@ -6,9 +6,6 @@
 }: let
   inherit (lib) types mkIf mkEnableOption;
   cfg = config.device.server.homelab;
-
-  scripts = import ./scripts.nix;
-  # homeassistantPackages = import ./pkgs {inherit pkgs;};
 in {
   options.device.server.homelab = {
     homeassistant = {
@@ -17,7 +14,6 @@ in {
   };
 
   config = mkIf cfg.homeassistant.enable {
-    # environment.systemPackages = [pkgs.home-assistant];
     services = {
       # nginx.virtualHosts."hass.joka00.dev" = {
       #   extraConfig = ''
@@ -32,86 +28,20 @@ in {
       #     proxyWebsockets = true;
       #   };
       # };
-      home-assistant = let
-      in {
-        enable = cfg.homeassistant.enable;
-        package = pkgs.home-assistant;
-        extraPackages = python3Packages:
-          with python3Packages; [
-            gtts
-            pyatv
+      virtualisation.oci-containers = {
+        backend = "podman";
+        containers.homeassistant = {
+          volumes = ["home-assistant:/config"];
+          environment.TZ = "Europe/Berlin";
+          image = "ghcr.io/home-assistant/home-assistant:stable";
+          extraOptions = [
+            "--network=host"
+            "--device=/dev/ttyACM0:/dev/ttyACM0"
           ];
-        extraComponents = [
-          # Components required to complete the onboarding
-          "cloud"
-          "broadlink"
-          "default_config"
-          "esphome"
-          "met"
-          "mqtt"
-          "mobile_app"
-          "homekit"
-          "homekit_controller"
-          "radio_browser"
-          "network"
-          "tailscale"
-          "tuya"
-          "samsungtv"
-          "system_health"
-          "system_log"
-          "update"
-          "websocket_api"
-          "upnp"
-          "zeroconf"
-          "zha"
-        ];
-        customComponents = [
-          pkgs.home-assistant-custom-components.adaptive_lighting
-          pkgs.home-assistant-custom-components.localtuya
-        ];
-        customLovelaceModules = with pkgs; [
-          home-assistant-custom-lovelace-modules.card-mod
-          home-assistant-custom-lovelace-modules.button-card
-          home-assistant-custom-lovelace-modules.mini-graph-card
-          home-assistant-custom-lovelace-modules.mushroom
-        ];
-        config = {
-          default_config = {};
-          config = {};
-          mobile_app = {};
-          cloud = {};
-          network = {};
-          zeroconf = {};
-          system_health = {};
-          system_log = {};
-          http = {
-            use_x_forwarded_for = true;
-            trusted_proxies = [
-              "127.0.0.1"
-              "::1"
-            ];
-          };
-          # Includes dependencies for a basic setup
-          # https://www.home-assistant.io/integrations/default_config/
-          script = import ./script.nix;
-          "automation lights" = import ./automations/automation-lights.nix;
-          "automation gate" = import ./automations/automation-gate.nix;
-          "automation utils" = import ./automations/automation-utils.nix;
-          "automation ui" = "!include automations.yaml";
         };
       };
     };
-    networking.firewall.allowedTCPPorts = [8123 21064];
-    networking.firewall.allowedUDPPorts = [21064];
-
-    systemd.tmpfiles.rules = [
-      "f ${config.services.home-assistant.configDir}/automations.yaml 0755 hass hass"
-    ];
-
-    environment.persistence = mkIf config.device.core.storage.enablePersistence {
-      "/persist" = {
-        directories = ["/var/lib/hass"];
-      };
-    };
+    networking.firewall.allowedTCPPorts = [8123 21063 21064];
+    networking.firewall.allowedUDPPorts = [5353 21063 21064];
   };
 }
