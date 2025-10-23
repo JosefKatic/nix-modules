6dcc67609b6ca5381a3ea2cdb05904827f53b772
Josef Katič <josef@joka00.dev>
2025-10-23 11:54:42 +0200

    Fix git changes

--- modules/home/user/terminal/programs/git/default.nix
+++ modules/home/user/terminal/programs/git/default.nix
@@ -5,52 +5,51 @@
 }: {
   home.packages = [pkgs.gh];
 
+  programs.delta = {
+    enable = true;
+    options.${config.programs.matugen.variant} = true;
+    enableGitIntegration = true;
+  };
+
   programs.git = {
     enable = true;
-
-    delta = {
-      enable = true;
-      options.${config.programs.matugen.variant} = true;
-    };
-
-    extraConfig = {
-      diff.colorMoved = "default";
-      merge.conflictstyle = "diff3";
-    };
-
-    aliases = {
-      a = "add";
-      b = "branch";
-      c = "commit";
-      ca = "commit --amend";
-      cm = "commit -m";
-      co = "checkout";
-      d = "diff";
-      ds = "diff --staged";
-      p = "push";
-      pf = "push --force-with-lease";
-      pl = "pull";
-      l = "log";
-      r = "rebase";
-      s = "status --short";
-      ss = "status";
-      forgor = "commit --amend --no-edit";
-      graph = "log --all --decorate --graph --oneline";
-      oops = "checkout --";
-    };
-
-    ignores = ["*~" "*.swp" "*result*" ".direnv" ".idea" ".vscode" "node_modules"];
-    userName = "Josef Katič";
-    userEmail = "josef@joka00.dev";
     signing = {
       key = "0xBAD7648677C2B3C6";
       signer = "${config.programs.gpg.package}/bin/gpg2";
       signByDefault = true;
     };
-    extraConfig = {
+    settings = {
+      alias = {
+        a = "add";
+        b = "branch";
+        c = "commit";
+        ca = "commit --amend";
+        cm = "commit -m";
+        co = "checkout";
+        d = "diff";
+        ds = "diff --staged";
+        p = "push";
+        pf = "push --force-with-lease";
+        pl = "pull";
+        l = "log";
+        r = "rebase";
+        s = "status --short";
+        ss = "status";
+        forgor = "commit --amend --no-edit";
+        graph = "log --all --decorate --graph --oneline";
+        oops = "checkout --";
+      };
+
+      ignores = ["*~" "*.swp" "*result*" ".direnv" ".idea" ".vscode" "node_modules"];
+      user = {
+        name = "Josef Katič";
+        email = "josef@joka00.dev";
+      };
       feature.manyFiles = true;
       init.defaultBranch = "main";
       commit.gpgSign = true;
+      diff.colorMoved = "default";
+      merge.conflictstyle = "diff3";
     };
     lfs.enable = true;
   };
